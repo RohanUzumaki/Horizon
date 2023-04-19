c1b0c71fc8bf13bb9bc377811c073a8a001a6348
Nizamul Haq S <nizamintruder2001@gmail.com>
2023-04-19 21:30:13 +0530

    connected firebase

--- app/src/main/java/com/example/horizon/MainActivity.java
+++ app/src/main/java/com/example/horizon/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +13,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
         h.postDelayed(() -> {
             Intent intent = new Intent(MainActivity.this, LoginActivity.class);
